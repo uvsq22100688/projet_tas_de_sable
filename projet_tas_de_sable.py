16b97e69d83cfd939950ce205f6a63e29073e0b6
Aissam BERRAHMANE <aissam.berrahmane@ens.uvsq.fr>
2022-03-09 15:08:12 +0100

    finition

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -11,6 +11,7 @@ import tkinter as tk
 import random
 import copy
 
+
 HEIGHT = 700
 WIDTH = 700
 grille = 20
@@ -37,7 +38,7 @@ def carre():
             color = "skyblue"
         if l3[i][2] == 0 :
             color = "powderblue"
-        obj.append(tk.Canvas.create_rectangle((l3[i][0]*tc,0+tc*l3[i][1]),(tc+l3[i][0]*tc,tc+tc*l3[i][1]),fill = ))
+        obj.append(canvas.create_rectangle((l3[i][0]*tc,0+tc*l3[i][1]),(tc+l3[i][0]*tc,tc+tc*l3[i][1]),fill = color,outline = color ))
 
 
 def sauvegarde():
@@ -79,7 +80,7 @@ def max_stable():
         l3[i][2] = 6
     for i in range(1,cases):
         color = "royalblue"
-        obj.append(tk.Canvas.create_rectangle((l3[i][0]*tc,0+tc*l3[i][1]),(tc+l3[i][0]*tc,tc+tc*l3[i][1]),fill = ))
+        obj.append(canvas.create_rectangle((l3[i][0]*tc,0+tc*l3[i][1]),(tc+l3[i][0]*tc,tc+tc*l3[i][1]),fill = color,outline = color))
 
 
 def clic_config(event):
@@ -88,19 +89,118 @@ def clic_config(event):
         if tc*l3[i][0] < event.x <tc+tc*l3[i][0] and tc*l3[i][1] < event.y< tc+tc*l3[i][1]:
             print(l3)
             for c in range (len(obj)):
-                tk.Canvas.delete(obj[c])
+                canvas.delete(obj[c])
             carre()
 
 
 def stop():
+    global cond
+    cond = False
 
 
-#def changement_config():
+def changement_config():
+    global l3 , obj , cond
+    l4 = copy.deepcopy(l3)
+    for i in range(1, cases):
+        if l4[i][0] == 0 and l4[i][1]==0 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i+1][2] +=1
+            l3[i+grille][2] +=1 
+
+        elif l4[i][0] == grille1 and l4[i][1]==0 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-1][2] +=1
+            l3[i+grille][2] +=1 
+
+        elif l4[i][0] == 0 and l4[i][1]==grille1 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i+1][2] +=1
+            l3[i-grille][2] +=1 
+
+        elif l4[i][0] == grille1 and l4[i][1]==grille1 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-1][2] +=1
+            l3[i-grille][2] +=1 
+        
+        elif l4[i][0] == 0 and  l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-grille][2] +=1
+            l3[i+grille][2] +=1 
+            l3[i+1][2] +=1
+
+        elif l4[i][0] == grille1 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-grille][2] +=1
+            l3[i+grille][2] +=1 
+            l3 [i-1][2] +=1
+
+        elif l4[i][1]==0 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i+grille][2] +=1
+            l3[i-1][2] +=1
+            l3[i+1][2] +=1
+
+        elif l4[i][1]==grille1 and l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-grille][2] +=1
+            l3[i+1][2] +=1
+            l3[i-1][2] +=1
+
+        elif  l4[i][2] >=4:
+            l3[i][2] -= 4
+            l3[i-grille][2] +=1
+            l3[i+grille][2] +=1
+            l3[i+1][2] +=1
+            l3[i-1][2] +=1
+        for c in range(len(obj)):
+            canvas.delete(obj[c])
+        obj = []
+        carre ()
+        if cond == True:
+            canvas.after(50, changement_config)  
+        elif cond == False:
+            cond = True
 
 
 
 
 
+racine = tk.Tk()
+canvas = tk.Canvas(racine , height = HEIGHT , width = WIDTH , bg = "black")
+canvas.pack(side='right')
+canvas.bind("<Button-1>",clic_config)
+
+bouton1 = tk.Button(racine,text="changemenet de configuration", command = changement_config)
+bouton1.pack(side = "top", fill = "x")
+
+bouton2 = tk.Button(racine,text="pile centrée", command = pile_centrée)
+bouton2.pack(side = "top", fill = "x")
+
+bouton3 = tk.Button(racine,text="configuration aleatoire", command = config_aleatoire)
+bouton3.pack(side = "top", fill = "x")
+
+bouton4 = tk.Button(racine,text="max stable", command = max_stable)
+bouton4.pack(side = "top", fill = "x")
+
+bouton5 = tk.Button(racine,text="sauvegarder", command = sauvegarde)
+bouton5.pack(side = "top", fill = "x")
+
+bouton6 = tk.Button(racine,text="afficher la sauvegarde", command = affiche_sauvegarde)
+bouton6.pack(side = "top", fill = "x")
+
+bouton7 = tk.Button(racine,text="Stop", command = stop)
+bouton7.pack(side = "top", fill = "x")
+
+for a in range(grille):
+    for v in range(grille):
+        l2=[]
+        l2.append(v)
+        l2.append(a)
+        l2.append(0)
+        l2.append(l2)
+        obj.append(canvas.create_rectangle((v*tc,tc*a),(tc+v*tc,tc+a*tc), fill = color,outline = color))
+
+    racine.mainloop
 
 
 
